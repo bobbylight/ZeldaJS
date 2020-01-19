86c9f4edbc522551f2bc1dad7e7e4b756f00ddba
bobbylight <robert.e.futrell@gmail.com>
2020-01-19 18:39:15 -0500

    Default to weak enemies if enemy strength is not defined in JSON

--- src/InstanceLoader.ts
+++ src/InstanceLoader.ts
@@ -3,11 +3,9 @@ import { EnemyStrength } from '@/enemy/Enemy';
 
 export class InstanceLoader {
 
-    static create<T>(className: string, strength?: EnemyStrength): T {
+    static create<T>(className: string, strength: EnemyStrength = 'red'): T {
         const instance: T = Object.create((EnemyTypes as any)[className].prototype);
-        if (strength) {
-            (instance as any).constructor.apply(instance, [ strength ]);
-        }
+        (instance as any).constructor.apply(instance, [ strength ]);
         return instance;
     }
 }
