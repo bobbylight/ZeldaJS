b92a4ba57ad03dfe11d7da9b1c2854797c05b216
bobbylight <robert.e.futrell@gmail.com>
2020-06-12 23:35:13 -0400

    Get unit tests running again

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverage: true
+  collectCoverage: true,
+  // A map from regular expressions to module names so our '@' root alias is known to Jest
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1"
+  }
 };
